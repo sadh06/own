dbb2700d753fb781a8ffe69f583f9b92d65e3a62
sadh06 <99791363+sadh06@users.noreply.github.com>
2022-02-22 14:30:48 +0530

    Add files via upload

---
+++ train-prev.py
@@ -0,0 +1,28 @@
+import numpy as np
+import tensorflow as tf
+
+input_path = 'mnist.npz'
+with np.load(input_path, allow_pickle=True) as f:
+    x_train, y_train = f['x_train'], f['y_train']
+    x_test, y_test = f['x_test'], f['y_test']
+
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+model.compile(optimizer='adam',
+              loss=loss_fn,
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+
+model.evaluate(x_test,  y_test, verbose=2)
+
+output_path = 'model.h5'
+model.save(output_path)
